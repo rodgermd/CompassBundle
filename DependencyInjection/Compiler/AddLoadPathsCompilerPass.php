d46060b1ef0642fa7c39b23068007e830eeff9dc
Alexander Murashov <rodger@ladela.com>
2013-12-19 15:37:05 +0200

    add doccomments

--- DependencyInjection/Compiler/AddLoadPathsCompilerPass.php
+++ DependencyInjection/Compiler/AddLoadPathsCompilerPass.php
@@ -6,14 +6,25 @@ use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 
+/**
+ * Class AddLoadPathsCompilerPass
+ * Adds source path for assetic
+ *
+ * @package Rodgermd\CompassBundle\DependencyInjection\Compiler
+ */
 class AddLoadPathsCompilerPass implements CompilerPassInterface
 {
-  public function process(ContainerBuilder $container)
-  {
-    $path = __DIR__ . '/Resources/assets/compass';
+    /**
+     * Processes config
+     *
+     * @param ContainerBuilder $container
+     */
+    public function process(ContainerBuilder $container)
+    {
+        $path = __DIR__ . '/Resources/assets/compass';
 
-    $compass_service_load_paths = $container->getParameter('assetic.filter.compass.load_paths');
-    $compass_service_load_paths[] = $path;
-    $container->setParameter('assetic.filter.compass.load_paths', $compass_service_load_paths);
-  }
+        $compass_service_load_paths   = $container->getParameter('assetic.filter.compass.load_paths');
+        $compass_service_load_paths[] = $path;
+        $container->setParameter('assetic.filter.compass.load_paths', $compass_service_load_paths);
+    }
 }
--- RodgermdCompassBundle.php
+++ RodgermdCompassBundle.php
@@ -7,12 +7,23 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Rodgermd\CompassBundle\DependencyInjection\Compiler\AddLoadPathsCompilerPass;
 
+/**
+ * Class RodgermdCompassBundle
+ * Bundle definition
+ *
+ * @package Rodgermd\CompassBundle
+ */
 class RodgermdCompassBundle extends Bundle
 {
-  public function build(ContainerBuilder $container)
-  {
-    parent::build($container);
+    /**
+     * On build
+     *
+     * @param ContainerBuilder $container
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
 
-    $container->addCompilerPass(new AddLoadPathsCompilerPass());
-  }
+        $container->addCompilerPass(new AddLoadPathsCompilerPass());
+    }
 }
